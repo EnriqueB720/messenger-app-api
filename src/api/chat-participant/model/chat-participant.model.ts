6c2538f4993a7603342826f9f61ae288be01a38c
EnriqueB720 <enrique21102001@hotmail.com>
2023-11-13 17:29:59 -0600

    adding user to chat participant model

--- src/api/chat-participant/model/chat-participant.model.ts
+++ src/api/chat-participant/model/chat-participant.model.ts
@@ -1,5 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Message } from 'src/api/message/model';
+import { User } from 'src/api/user/model/user.model';
 
 @ObjectType()
 export class ChatParticipant {
@@ -8,4 +9,7 @@ export class ChatParticipant {
 
   @Field(() => Number, { nullable: true })
   userId?: number;
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
