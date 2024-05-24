b0beec1fe88df2c973a715653326b0827fe08917
EnriqueB720 <enrique21102001@hotmail.com>
2024-05-24 15:48:33 -0600

    Adding api index

---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+import { UserModule } from './user/user.module';
+import { ContactModule } from './contact/contact.module';
+import { ChatModule } from './chat/chat.module';
+import { MessageModule } from './message/message.module';
+import { ChatParticipantModule } from './chat-participant/chat-participant.module';
+import { UserMessageStatusModule } from './userMessageStatus/user-message-status.module';
+
+export {UserModule, ContactModule, ChatModule, MessageModule, ChatParticipantModule, UserMessageStatusModule}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { UserModule } from './api/user/user.module';
-import { ContactModule } from './api/contact/contact.module';
-import { ChatModule } from './api/chat/chat.module';
-import { MessageModule } from './api/message/message.module';
-import { ChatParticipantModule } from './api/chat-participant/chat-participant.module';
+import { ChatModule, ChatParticipantModule, ContactModule, MessageModule, UserMessageStatusModule, UserModule } from '@apis';
 
 import { ConfigModule } from 'src/shared/config/config.module';
 import { PrismaModule } from './shared/datasource/prisma/prisma.module';
-import { UserMessageStatusModule } from './api/userMessageStatus/user-message-status.module';
+
 //Hacer el index para los apis
 
 
